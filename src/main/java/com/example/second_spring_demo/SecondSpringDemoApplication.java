2d8e5a7e40243bb299460ed2a17aa807ccac0071
noahmancino <noahdanielmancino@gmail.com>
2022-04-18 16:10:38 -0400

    init, kinda.

---
+++ src/main/java/com/example/second_spring_demo/SecondSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.second_spring_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/second_spring_demo/SecondSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_spring_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
